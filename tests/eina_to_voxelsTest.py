1bce2691d9bc89402341d9d84c238f27ed59811b
Jorge Sanz <jorge.sanz.alcaine@gmail.com>
2017-09-06 14:02:03 +0200

    test para imagenes

--- tests/eina_to_voxelsTest.py
+++ tests/eina_to_voxelsTest.py
@@ -6,19 +6,14 @@ import eina_to_voxels
 import unittest
 
 class TestEina_to_voxels(unittest.TestCase):
-    """
-    def setUp(self):
-        self.world = eina_to_voxels.World("resources/EINA.las")
-    """
-        
         
     def testBasic(self):
-        self.world = eina_to_voxels.World("resources/EINA.las")
+        self.world = eina_to_voxels.World("resources/EINA.las",None,None,None,None,None)
         self.world.exportWorld("resources/out.txt")
     
     
     def testHeuristic(self):
-        self.world = eina_to_voxels.World("resources/EINA.las")
+        self.world = eina_to_voxels.World("resources/EINA.las",None,None,None,None,None)
         
         
         self.world.add(eina_to_voxels.UseOpenStreetMap([]))
@@ -34,37 +29,6 @@ class TestEina_to_voxels(unittest.TestCase):
         
         self.world.start()
         self.world.exportWorld("resources/out.txt")
-    
-    """
-        
-    def testOpenStreetworld(self):
-        self.world.read("resources/EINA.las")
-        self.world.useOpenStreetMap()
-        self.world.exportWorld("resources/out.txt")
-        
-        
-    def testFull(self):
-        self.world.read("resources/EINA.las")
-        self.world.useOpenStreetMap()
-        self.world.useHeuristic()
-        self.world.exportWorld("resources/out.txt")
-
-    
-    def testAddBuilding(self):
-        self.world.read("resources/EINA.las")
-        self.world.addBuilding("resources/adaByron.binvox",62,49,5)
-        self.world.addSign(5,5)
-        self.world.exportWorld("resources/out.txt")
-        
-        
-    def testAddBuildingWithHeuristic(self):
-        self.world.read("resources/EINA.las")
-        self.world.addBuilding("resources/adaByron.binvox",62,49,5)
-        self.world.useHeuristic()
-        self.world.createWalls()
-        self.world.exportWorld("resources/out.txt")
-    """
-
 
 if __name__ == '__main__':
     unittest.main()
